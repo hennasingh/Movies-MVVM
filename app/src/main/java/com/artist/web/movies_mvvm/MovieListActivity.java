cd9b23a60b8ad743e134831167fb076c0eb1aafc
WEBARTIST\User <hennasingh04@gmail.com>
2018-11-12 23:58:02 +0000

    added menu resource file and option select methods in list activity fpr selecting movie list category.

--- app/src/main/java/com/artist/web/movies_mvvm/MovieListActivity.java
+++ app/src/main/java/com/artist/web/movies_mvvm/MovieListActivity.java
@@ -1,7 +1,9 @@
 package com.artist.web.movies_mvvm;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MovieListActivity extends AppCompatActivity {
 
@@ -9,5 +11,37 @@ public class MovieListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movie_list);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.category_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        String preference;
+        switch (item.getItemId()){
+            case R.id.top_rated:
+                preference = "top_rated";
+                break;
+            case R.id.now_playing:
+                preference = "now_playing";
+                break;
+            case R.id.popular:
+                preference ="popular";
+                break;
+            case R.id.upcoming:
+                preference ="upcoming";
+                break;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+        displayResults(preference);
+        return true;
+    }
+
+    private void displayResults(String preference) {
     }
 }
